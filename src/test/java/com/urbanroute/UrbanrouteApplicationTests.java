0191d11c9e51fdb8763e16762826e86b27a15293
Anna <anna.andersson.java25@edu.edugrade.se>
2026-06-03 12:56:54 +0200

    chore: initialize Spring Boot project

---
+++ src/test/java/com/urbanroute/UrbanrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
